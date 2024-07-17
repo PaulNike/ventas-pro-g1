4731c75dfd1fb09a3a9a3f5dfcc627d0467849c0
NikeRodriguez <nike_1109@outlook.es>
2024-07-17 04:03:13 -0500

    feat: project final g1 - arq. orientada al servicio

--- src/main/java/com/codigo/msventaspro/MsVentasProApplication.java
+++ src/main/java/com/codigo/msventaspro/MsVentasProApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class MsLoginApplication {
+public class MsVentasProApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsLoginApplication.class, args);
+		SpringApplication.run(MsVentasProApplication.class, args);
 	}
 
 }
--- src/test/java/com/codigo/msventaspro/MsVentasProApplicationTests.java
+++ src/test/java/com/codigo/msventaspro/MsVentasProApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MsLoginApplicationTests {
+class MsVentasProApplicationTests {
 
 	@Test
 	void contextLoads() {
